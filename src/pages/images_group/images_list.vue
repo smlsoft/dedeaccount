de41178630973a5a9b2386125b143332c49d35eb
NONTAWAT  WONGNUK <bos_catdog@hotmail.com>
2022-11-29 16:21:34 +0700

    - เพิ่ม log รวมจำนวนรูปในแต่ละวัน

--- src/pages/images_group/images_list.vue
+++ src/pages/images_group/images_list.vue
@@ -25,6 +25,7 @@ const route = useRoute();
 const toast = useToast();
 const detail = ref();
 const data_list = ref([]);
+const data_list_group = ref([]);
 const data_set_group = ref([]);
 const confirmSaveDialog = ref(false);
 const confirmGroupImageDialog = ref(false);
@@ -136,6 +137,7 @@ onUnmounted(() => {
 
 onMounted(() => {
   getDocumentImageGroup();
+  getDocumentImageGroupAll();
 
   storeApp.setActivePage("pic_group_docref");
   storeApp.setActiveChild("images_list");
@@ -380,6 +382,43 @@ function getDocumentImageGroupScroll() {
     .catch((err) => {
       console.log(err);
       showSkeleton.value = false;
+    });
+}
+
+function getDocumentImageGroupAll() {
+
+  ImageDataService.getDocumentImageGroupAll()
+    .then((res) => {
+      //console.log(res);
+      if (res.success) {
+        data_list_group.value = res.data;
+
+        data_list_group.value.forEach(element => {
+
+          element.uploadedat = Utils.getDateFormatDMY(element.uploadedat)
+        });
+
+          // group by uploaddate
+          const result = data_list_group.value.reduce(
+            (r, { uploadedat: uploadedat, ...object }) => {
+              var temp = r.find((o) => o.uploadedat === uploadedat);
+              if (!temp) r.push((temp = { uploadedat, children: [] }));
+              temp.children.push(object);
+              return r;
+            },
+            []
+          );
+          
+          console.log(result);
+      }
+    })
+    .catch((err) => {
+      toast.add({
+        severity: "error",
+        summary: "Error",
+        detail: err,
+        life: 3000,
+      });
     });
 }
 
@@ -2342,5 +2381,15 @@ function documentImageEditGroup(data) {
   position: fixed;
   top: 0;
   width: 100%;
+}
+
+@media only screen and (max-width: 991px) {
+  .sticky-custom {
+    z-index: 999;
+    margin-top: 70px;
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
 }
 </style>
