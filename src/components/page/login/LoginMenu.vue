b244e09f574b27911bbd27e245e3e82d2122ef43
NONTAWAT  WONGNUK <bos_catdog@hotmail.com>
2024-01-31 10:30:35 +0700

    update

--- src/components/page/login/LoginMenu.vue
+++ src/components/page/login/LoginMenu.vue
@@ -30,23 +30,24 @@ function loginWithGoogle() {
         </div>
         <languageButton />
       </div>
-      <div class="flex justify-content-center">
+      <div class="flex justify-content-center mt-6">
         <img
           src="@/assets/img/dedeaccount_logo.png"
           alt="Image"
-          height="100"
-          width="100"
+          height="200"
+          width="200"
           class="mb-3"
         />
       </div>
-      <Button
-        @click="loginWithGoogle"
-        class="w-12 font-medium border-1 surface-border surface-100 py-2 px-5 p-component hover:surface-200 active:surface-300 text-900 cursor-pointer transition-colors transition-duration-150 inline-flex align-items-center"
-      >
-        <i class="pi pi-google text-red-500" style="font-size: 20px"></i>
-        <span class="ml-2">{{ $t("singin_with") }} Google</span>
-      </Button>
-
+      <div class="flex justify-content-center">
+        <Button
+          @click="loginWithGoogle"
+          class="w-full mt-3 font-medium border-1 surface-border surface-100 py-2 px-5 p-component hover:surface-200 active:surface-300 text-900 cursor-pointer transition-colors transition-duration-150 align-items-center justify-content-center"
+        >
+          <i class="pi pi-google text-red-500" style="font-size: 20px"></i>
+          <span class="ml-2">{{ $t("singin_with") }} Google</span>
+        </Button>
+      </div>
       <!-- <div class="flex align-items-center justify-content-end mt-2">
         <a
           @click="loginMode()"
